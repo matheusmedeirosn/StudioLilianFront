623a9d7f10121e73d2f2d97c5d37f976ea17ac9a
matheusmedeirosn <matheusmedeiroosn@gmail.com>
2024-02-18 10:35:48 -0300

    ajuste modal edição não aparece

--- consultarAgendamento.js
+++ consultarAgendamento.js
@@ -99,6 +99,9 @@ function abrirFormularioEdicao(button) {
                 document.getElementById('dataEdit').value = agendamento.data; // Formato AAAA-MM-DD
                 document.getElementById('horarioEdit').value = agendamento.horario; // Formato HH:MM
 
+                // Aplicar máscaras aos procedimentos selecionados
+                $('#procedimentosEdit').selectpicker('refresh');
+
                 $('#modalEdicaoAgendamento').modal('show');
 
                 // Ao abrir o formulário de edição, defina o ID do agendamento no campo oculto
